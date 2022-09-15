3f89832f084277f7ce5516db13d03742f7090909
i-guerrero <isaacguerrero@pursuit.org>
2022-09-14 20:21:37 -0400

    Answer #1

--- js-practice.js
+++ js-practice.js
@@ -1,31 +1,34 @@
-//Complete the instructions for each numbered problem 
+//Complete the instructions for each numbered problem
 
-//1. data types built-in methods, variables 
+//1. data types built-in methods, variables
 
 //a. define a variable and give it a string value
+let name = "Isaac";
 
 //b. define a variable and give it a number value
+let age = 25;
 
 //c. define a variable and give it a boolean value
+let isProgrammer = true;
+console.log(isProgrammer);
 
-//2. if else, ternary 
+//2. if else, ternary
 //a. define a variable 'metrocard' and give it a value of 5
 
 //b. write an if statement that has a condition to check if the value of metro card is greater than 2.75, and display the message "you have enough to ride the train", if the condition is true
 
 //c.write an else statement that displays the message "you do not have enough to pay fare sorry"
 
-//d. write the above if/else statement again as a ternary 
+//d. write the above if/else statement again as a ternary
 
 //3. conditionals 2 (1 pt)
-//a.  complete this codecademy project: https://www.codecademy.com/courses/introduction-to-javascript/projects/magic-eight-ball-1 
+//a.  complete this codecademy project: https://www.codecademy.com/courses/introduction-to-javascript/projects/magic-eight-ball-1
 //add a screenshot showing your completion of this project to the root folder of this repo
 
-//4. functions 
-//a. write a function named helloWorld that returns the value 'Hello World!' 
+//4. functions
+//a. write a function named helloWorld that returns the value 'Hello World!'
 
 //b. console log the value returned from running the function helloWorld
-
 
 //3. arrays
 //a. define a variable myArray and assign it to a value of an array. Give the array at least 5 elements
@@ -33,19 +36,17 @@
 //b. an array's index starts at:
 //
 
-
-//4. loops 
-//a. write a for loop that console logs even numbers starting at 10 and decrementing to 0 
-
+//4. loops
+//a. write a for loop that console logs even numbers starting at 10 and decrementing to 0
 
 //5. objects
-//a. define a variable named 'bike' 
+//a. define a variable named 'bike'
 
-//b. assign an object to bike 
+//b. assign an object to bike
 
-//c. give the object 3 properties: handlebars, color, wheels 
+//c. give the object 3 properties: handlebars, color, wheels
 
-//d. give each property a value 
+//d. give each property a value
 
 //6. Chessboard
 /*Write a function that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
@@ -64,7 +65,3 @@ When you have a program that generates this pattern, define a binding size = 8 a
 
 read and refer to https://eloquentjavascript.net/02_program_structure.html  there are hints at the bottom of the page 
 */
-
-
-
-
